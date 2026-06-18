dfb2aef52f607be8595c4e818d3fa9cf2865efd3
Asefa Negash <aseaau21@gmail.com>
2026-06-18 09:50:53 +0300

    feat(api): add enrollment CRUD controller endpoints

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd4eecbe26cb40690b3ae563cbab7a9e85ba709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47bfe95d4dc3d1069b723eecb52d8c5a64d5ac16")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
