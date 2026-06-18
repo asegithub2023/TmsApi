96686e71ed18be83604eef1b8c4ae740342aaa25
Asefa Negash <aseaau21@gmail.com>
2026-06-18 18:30:49 +0300

    feat(api): add student and course endpoints

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("students")]
+public class StudentsController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public IActionResult GetStudentById(string id)
+    {
+        return Ok(new
+        {
+            Id = id,
+            Name = "Abebe",
+            Department = "Computer Science"
+        });
+    }
+
+    [HttpGet("all")]
+    public IActionResult GetAllStudents()
+    {
+        return Ok(new[]
+        {
+            new { Id = "S001", Name = "Abebe" },
+            new { Id = "S002", Name = "Kebede" }
+        });
+    }
+}
